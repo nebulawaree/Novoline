372705260952cd14f3704de155934b8de224d9d9
XzynAstralz <xzynggx@gmail.com>
2024-06-08 04:36:57 -0500

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -15,7 +15,7 @@ local GuiLibrary = loadstring(readfile("Aristois/GuiLibrary.lua"))()
 local WhitelistModule = loadstring(readfile("Aristois/Librarys/Whitelist.lua"))()
 local boxHandleAdornment = Instance.new("BoxHandleAdornment")
 local IsOnMobile = table.find({Enum.Platform.IOS, Enum.Platform.Android}, UserInputService:GetPlatform())
-
+print("waeaeae")
 local Table = {
     ChatStrings1 = {
         ["HYPE73WZNQRT5"] = "Aristois",

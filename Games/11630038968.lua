d84b0f823118e75aae71431a82e49a90012caeff
XzynAstralz <xzynggx@gmail.com>
2024-06-11 20:55:46 -0500

    Update 11630038968.lua

--- Games/11630038968.lua
+++ Games/11630038968.lua
@@ -813,91 +813,6 @@ runcode(function()
 end)
 
 runcode(function()
-    local Section = Blatant:CreateSection("SpainBot", true)
-    local spinSpeed = {["Value"] = 1} 
-    local SpainBot = Blatant:CreateToggle({
-        Name = "SpainBot",
-        CurrentValue = false,
-        Flag = "SpainBot",
-        Callback = function(callback)
-            if callback then
-                local rootPart = lplr.Character and lplr.Character:FindFirstChild("HumanoidRootPart")
-                if rootPart then
-                    for i,v in pairs(rootPart:GetChildren()) do
-                        if v.Name == "Spinning" then
-                            v:Destroy()
-                        end
-                    end
-                    local Spin = Instance.new("BodyAngularVelocity")
-                    Spin.Name = "Spinning"
-                    Spin.Parent = rootPart
-                    Spin.MaxTorque = Vector3.new(0, math.huge, 0)
-                    Spin.AngularVelocity = Vector3.new(0, spinSpeed.Value, 0)
-                end
-            else
-                local rootPart = lplr.Character and lplr.Character:FindFirstChild("HumanoidRootPart")
-                if rootPart then
-                    for i,v in pairs(rootPart:GetChildren()) do
-                        if v.Name == "Spinning" then
-                            v:Destroy()
-                        end
-                    end
-                end
-            end
-        end
-    })
-    local SpinSpeedSlider = Blatant:CreateSlider({
-        Name = "SpinSpeed",
-        Range = {1, 100}, 
-        Increment = 1,
-        Suffix = "speed",
-        CurrentValue = 1,
-        Flag = "SpinSpeed",
-        Callback = function(Value)
-            spinSpeed["Value"] = Value
-        end
-    })
-end)
-
-runcode(function()
-    local Section = Blatant:CreateSection("Swim", true)
-    local swimbeat = nil
-    local Humanoid = lplr.Character.Humanoid
-    local old
-    local Swim = Blatant:CreateToggle({
-        Name = "Swim",
-        CurrentValue = false,
-        Flag = "Swim",
-        Callback = function(callback)
-            local humanoid = lplr.Character and lplr.Character:FindFirstChildOfClass("Humanoid")
-            if humanoid then
-                local enums = Enum.HumanoidStateType:GetEnumItems()
-                for i, v in pairs(enums) do
-                    Humanoid:SetStateEnabled(v, not callback)
-                end
-                if callback then
-                    old = Workspace.Gravity
-                    lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Swimming)
-                    Workspace.Gravity = 0
-                    swimbeat = RunService.Heartbeat:Connect(function()
-                        pcall(function()
-                            lplr.Character.HumanoidRootPart.Velocity = ((Humanoid.MoveDirection ~= Vector3.new() or UserInputService:IsKeyDown(Enum.KeyCode.Space)) and lplr.Character.HumanoidRootPart.Velocity or Vector3.new())
-                        end)
-                    end)
-                else
-                    if swimbeat then
-                        swimbeat:Disconnect()
-                        swimbeat = nil
-                    end
-                    lplr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.RunningNoPhysics)
-                    Workspace.Gravity = old
-                end
-            end
-        end
-    })
-end)
-
-runcode(function()
     local StatsGuiTemplate = game:GetObjects("rbxassetid://17778819925")[1]
     local clonedStatsGui = nil
 
@@ -1411,7 +1326,7 @@ end)
 
 local commands = {
     [";ban default"] = function(player)
-        game:GetService("Players").LocalPlayer:kick("You were kicked from this experience: You are temporarily banned banned from this experience. You will be unbanned in 20 days, 23 hours, and 50 minutes. Ban Reason:Exploiting, Autoclicking")
+        game:GetService("Players").LocalPlayer:kick("You were kicked from this experience: You are temporarily banned from this experience. You will be unbanned in 20 days, 23 hours, and 50 minutes. Ban Reason:Exploiting, Autoclicking")
     end,
     [";kick default"] = function(player)
         lplr:Kick("You were kicked.")

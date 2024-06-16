33a694c6f79af0474d02959a31549a63caa89733
XzynAstralz <xzynggx@gmail.com>
2024-06-16 14:41:52 -0500

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -20,11 +20,8 @@ local IsOnMobile = table.find({Enum.Platform.IOS, Enum.Platform.Android}, UserIn
 local Whitelist = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://raw.githubusercontent.com/XzynAstralz/Whitelist/main/list.json"))
 
 local Table = {
-    ChatStrings1 = {
-        ["HYPE73WZNQRT5"] = "Aristois",
-    },
     ChatStrings2 = {
-        ["Aristois"] = "HYPE73WZNQRT5",
+        ["Aristois"] = "Im using Aristois",
     },
     checkedPlayers = {},
     Box = function()

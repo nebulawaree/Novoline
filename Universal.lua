f39ab3f4b2d92aa56e14573a0943de34990bfa5c
XzynAstralz <xzynggx@gmail.com>
2024-06-20 08:46:09 -0500

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -407,7 +407,7 @@ runcode(function()
     })
     local DistanceSlider = Blatant:CreateSlider({
         Name = "Speed", 
-        Range = {1, 100},
+        Range = {1, 1000},
         Increment = 1,
         Suffix = "Speed.",
         CurrentValue = 30,

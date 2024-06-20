8a4d3282c6cc12bd58f286c30819afb9bd6d141e
XzynAstralz <xzynggx@gmail.com>
2024-06-20 17:04:08 -0500

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -1,5 +1,5 @@
 local queueonteleport = (syn and syn.queue_on_teleport) or queue_for_teleport or queue_on_teleport or queueonteleport
-
+local GuiLibrary = loadstring(readfile("Aristois/GuiLibrary.lua"))()
 local bedwarsidtable = {
     6872274481,
     8444591321,
@@ -14,7 +14,6 @@ local BedWarsgame = table.find(bedwarsidtable, game.PlaceId)
 local BridgeDuelgame = table.find(bridgeduelidtable, game.PlaceId)
 shared.AristoisPlaceId = ""
 shared.SwitchServers = false 
-
 if BedWarsgame then 
     shared.AristoisPlaceId = 6872274481
 elseif BridgeDuelgame then
@@ -27,31 +26,32 @@ assert(not shared.Executed, "Already Injected")
 shared.Executed = true
 
 if shared.AristoisPlaceId == 6872274481 or shared.AristoisPlaceId == 11630038968 then
-    if shared.ReadFile then
-        loadstring(readfile("Aristois/Games/" .. shared.AristoisPlaceId .. ".lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/XzynAstralz/Aristois/main/Games/" .. shared.AristoisPlaceId .. ".lua"))()
-    end
+    loadstring(readfile("Aristois/Games/" .. shared.AristoisPlaceId .. ".lua"))()
 else
     local placeFilePrivate = "Aristois/Games/" .. tostring(shared.AristoisPlaceId) .. ".lua"
     if isfile(placeFilePrivate) then
         loadstring(readfile(placeFilePrivate))()
     else
-        if shared.ReadFile then
-            loadstring(readfile("Aristois/Universal.lua"))()
-        else
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/XzynAstralz/Aristois/main/Universal.lua"))()
-        end
+        loadstring(readfile("Aristois/Universal.lua"))()
     end
 end
 
+local configLoop = coroutine.create(function()
+    repeat
+        GuiLibrary.SaveConfiguration()
+        task.wait(10)
+    until shared.SwitchServers or not shared.Executed
+end)
+
+coroutine.resume(configLoop)
+
 local ServerSwitchScript = [[
     shared.SwitchServers = true 
-    if shared.ReadFile then 
-        loadstring(readfile("Aristois/NewMainScript.lua"))() 
-    else 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/XzynAstralz/Aristois/main/NewMainScript.lua", true))() 
-    end
+    loadstring(readfile("Aristois/NewMainScript.lua"))()
 ]]
+
+if shared.SwitchServers then
+    GuiLibrary.SaveConfiguration()
+end
 
 queueonteleport(ServerSwitchScript)
